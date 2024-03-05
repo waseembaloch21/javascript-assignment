ac0bd90d9201b8a6bb6dc596981fbfe1cde39d4b
waseembaloch21 <154684340+waseembaloch21@users.noreply.github.com>
2024-03-05 15:19:42 +0500

    code update

---
+++ assigment4/app.js
@@ -0,0 +1,31 @@
+
+//chapter6
+
+//answer1
+
+var num = 10;
+document.write("<h2>The value of a is:</h2>" + num );
+
+
+var num = 10;
+var newNums = ++num
+document.write("<h2>The value of ++a is:</h2>" + newNums);
+document.write("<h2>Now the value of is:</h2>" + newNums);
+
+var nums = 11;
+var newNums = 11+1
+document.write("<h2>The value of a++ is:</h2>" + nums);  
+document.write("<h2> Now the value of a is:</h2>" + newNums);
+
+var num = 12;
+var newsNums = 12-1
+document.write("<h2>The value of --a is:</h2>" + newsNums);
+document.write("<h2> Now the value of a is:</h2>" + newsNums);
+
+var num = 11
+var newsNums = 11-1
+document.write("<h2>The value of a-- is:</h2>" + num);
+document.write("<h2> Now the value of a is:</h2>" + newsNums);
+
+
+
