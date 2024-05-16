c287219101da4dc41c452935e062e78b58156558
waseembaloch21 <154684340+waseembaloch21@users.noreply.github.com>
2024-05-16 14:12:01 +0500

    code update

---
+++ assigment13/app.js
@@ -0,0 +1,52 @@
+let mainImg = document.getElementById('watch_img')
+let img1 = document.getElementById('img1')
+let img2 = document.getElementById('img2')
+let img3 = document.getElementById('img3')
+let img4 = document.getElementById('img4')
+let img5 = document.getElementById('img5')
+
+
+img1.addEventListener('mouseover', function(){
+    mainImg.src = img1.src
+
+})
+img1.addEventListener('mouseout', function(){
+    mainImg.src = "https://cdn.shopify.com/s/files/1/0003/5815/4293/files/SV-11268_CreativeShot_M2-1.jpg?v=1694764453"
+})
+
+
+
+img2.addEventListener('mouseover', function(){
+    mainImg.src = img2.src
+
+})
+img2.addEventListener('mouseout', function(){
+    mainImg.src = "https://cdn.shopify.com/s/files/1/0003/5815/4293/files/SV-11268_CreativeShot_M2-1.jpg?v=1694764453"
+})
+
+
+
+img3.addEventListener('mouseover', function(){
+    mainImg.src = img3.src
+})
+img3.addEventListener('mouseout', function(){
+    mainImg.src = "https://cdn.shopify.com/s/files/1/0003/5815/4293/files/SV-11268_CreativeShot_M2-1.jpg?v=1694764453"
+})
+
+
+
+img4.addEventListener('mouseover', function(){
+    mainImg.src = img4.src
+})
+img4.addEventListener('mouseout', function(){
+    mainImg.src = "https://cdn.shopify.com/s/files/1/0003/5815/4293/files/SV-11268_CreativeShot_M2-1.jpg?v=1694764453"
+})
+
+
+
+img5.addEventListener('mouseover', function(){
+    mainImg.src = img5.src
+})
+img5.addEventListener('mouseout', function(){
+    mainImg.src = "https://cdn.shopify.com/s/files/1/0003/5815/4293/files/SV-11268_CreativeShot_M2-1.jpg?v=1694764453"
+})
