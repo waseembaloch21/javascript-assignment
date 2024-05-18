587fddeafd2aba34e23396901901f52b8121e9e1
waseembaloch21 <154684340+waseembaloch21@users.noreply.github.com>
2024-05-18 17:14:11 +0500

    code update

---
+++ assigment14/app.js
@@ -0,0 +1,27 @@
+let h1 = document.getElementById('heading')
+let para = document.getElementById('text')
+
+function bold() {
+    heading.style.fontWeight='900'
+    text.style.fontWeight='800'
+}
+
+function italic() {
+    heading.style.fontStyle='italic'
+    text.style.fontStyle='italic'
+}
+
+function underLine() {
+    heading.style.textDecoration='underline'
+    text.style.textDecoration='underline'
+}
+
+function align_left() {
+    heading.style.textAlign='left'
+    text.style.textAlign='left'
+}
+
+function align_right() {
+    heading.style.textAlign='right'
+    text.style.textAlign='right'
+}
